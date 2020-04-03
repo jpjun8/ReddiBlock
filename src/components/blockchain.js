9304d43006c406390f6caa1198805597e7db57d1
Joonpyo Jun <jpjun8@gmail.com>
2020-04-03 17:31:57 -0400

    some changes

--- src/components/blockchain.js
+++ src/components/blockchain.js
@@ -26,7 +26,7 @@ export default class CryptoBlockChain extends React.Component {
         let smashingCoin = new CryptoBlockChain();
         smashingCoin.addNewBlock(new CryptoBlock(1, "01/06/2020", {sender: "Iris Ljesnjanin", recipient: "Cosima Mielke", quantity: 50}));
         smashingCoin.addNewBlock(new CryptoBlock(2, "01/07/2020", {sender: "Vitaly Friedman", recipient: "Ricardo Gimenes", quantity: 100}));
-        console.log(JSON.stringify(smashingCoin, null, 4));
+        console.log(JSON.stringify(smashingCoin.blockchain, null, 4));
     }
 
     render() {
